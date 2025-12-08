150089751a3f9cd7cf9e07a1e9d5ff8726e8ceab
Маргарита Филинова <margofilinova2006@gmail.com>
2025-12-08 15:48:35 +0500

    module 8 task 1

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,19 +1,29 @@
+import { openBigPicture } from './big-picture.js';
+
 const picturesContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const createThumbnail = (photo) => {
+  const thumbnail = pictureTemplate.cloneNode(true);
+  const image = thumbnail.querySelector('.picture__img');
+  image.src = photo.url;
+  image.alt = photo.description;
+
+  thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = photo.likes;
+
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openBigPicture(photo);
+  });
+  return thumbnail;
+};
 
 const renderThumbnails = (photos) => {
   const fragment = document.createDocumentFragment();
 
   photos.forEach((photo) => {
-    const thumbnail = pictureTemplate.cloneNode(true);
-
-    const image = thumbnail.querySelector('.picture__img');
-    image.src = photo.url;
-    image.alt = photo.description;
-
-    thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
-    thumbnail.querySelector('.picture__likes').textContent = photo.likes;
-
+    const thumbnail = createThumbnail(photo);
     fragment.appendChild(thumbnail);
   });
 
